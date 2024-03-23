5c8738094b2781cd896553bcd14039ef79215fbf
t3komu00 <your.email@example.com>
2024-03-23 23:57:18 +0200

    Completed task 07 and working

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001';
+//const BACKEND_ROOT_URL = 'https://todo-backend-3d5g.onrender.com';
 import {Todos} from './class/Todos.js';
 
 const todos = new Todos(BACKEND_ROOT_URL);
